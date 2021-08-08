11a4e8039b7626fd0718b2468c58a057c9037d80
Roman <glasbartgames@gmail.com>
2021-08-08 21:04:46 +0200

    match ui

--- scripts/matchengine/Matchengine.gd
+++ scripts/matchengine/Matchengine.gd
@@ -1,11 +1,15 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+var dict = {
+	"strength" : 10,
+	"dexterity" : 10,
+	"agility" : 10,
+	"stamina" : 10,
+	"intelligence" : 10,
+	"speed" : 10,
+	"charisma" : 10,
+	"luck" : 10
+}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
